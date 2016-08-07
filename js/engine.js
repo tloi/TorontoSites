e289508e7c1b4542fedd6338e75a73bbefa05200
T Loi <tloi@hotmail.com>
2016-08-07 00:20:19 -0400

    y

--- js/engine.js
+++ js/engine.js
@@ -1,4 +1,105 @@
 ﻿'use strict'
+var geocoder = new google.maps.Geocoder();
+// Create a map object and specify the DOM element for display.
+var map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+        lat: 43.6534922,
+        lng: -79.3839729
+    },
+    scrollwheel: false,
+    zoom: 6
+});
+
+
+function getLongLat(address, callback) {
+    geocoder.geocode({
+        'address': address
+    }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            callback({
+                lat: results[0].geometry.location.lat(),
+                lng: results[0].geometry.location.lng()
+            });
+        }
+    });
+}
+function markSite(site) {
+    if (site.geo) {
+        var marker = new google.maps.Marker({
+            position: site.geo,
+            map: map,
+            title: site.address
+        });
+        getWikiInfo(site.wikiName, function (data) {
+            site.infoWindow = new google.maps.InfoWindow({
+                content: data
+            });
+            marker.addListener('click', function () {
+                site.infoWindow.open(map, marker);
+            });
+        });
+
+
+        return marker;
+    };
+    return null;
+
+}
+
+function clearMarker(sites) {
+    for (var x = 0; x < sites.length; x++) {
+        if (sites[x].marker) sites[x].marker.setMap(null);
+    }
+}
+
+
+function markSites(sites, map) {
+    for (var x = 0; x < sites.length; x++) {
+        if (sites[x].marker) sites[x].marker.setMap(map);
+    }
+}
+
+function compare(a, b) {
+    if (a.name < b.name)
+        return -1;
+    if (a.name > b.name)
+        return 1;
+    return 0;
+}
+
+var Site = function (name, address, wikiName) {
+    var self = this;
+    self.name = name;
+    self.address = address;
+    self.wikiName = wikiName;
+    getLongLat(address, function (data) {
+        self.geo = data;
+        self.marker = markSite(self);
+    });
+    return self;
+};
+
+function getWikiInfo(name, callback) {
+
+    var wikiURL = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&explaintext&format=json&formatversion=2&callback=?&titles=' + name;
+
+    $.getJSON(wikiURL, function (data) {
+        var info = ''
+        $.each(data.query.pages, function (i, item) {
+            if (item.extract)
+                info = info + '<p>' + item.extract + '</p>';
+            else {
+                info = '<p> No Information found</p>';
+            }
+        });
+        callback(info);
+    },
+        function (er) {
+            callback('Information not currently available');
+        }
+    );
+
+}
 
 // Overall viewmodel for this screen, along with initial state
 function OntarioViewModel(sites) {
